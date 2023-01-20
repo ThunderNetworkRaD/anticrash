bd7ccf4f2db097538bd7278890f22fc0124bac10
Killer Boss Original <73131550+killerbossoriginal@users.noreply.github.com>
2023-01-20 21:47:21 +0100

    Add process main

---
+++ index.js
@@ -0,0 +1,26 @@
+async function catchError (message, error, origin) {
+    
+}
+
+module.exports = {
+    start: ({disabled, enabled}) => {
+        if (!disabled.includes('unhandledRejection')) {
+            process.on('unhandledRejection', (err, origin) => {
+                catchError('Unhandled Rejection/Catch', err, origin);
+            });
+        }
+        if (!disabled.includes('uncaughtException')) {
+            process.on('uncaughtException', (err, origin) => {
+                catchError('Uncaught Exception/Catch', err, origin);
+            });
+        }
+        if (enabled.includes('multipleResolves')) {
+            process.on('multipleResolves', (type, promise) => {
+                catchError('Multiple Resolves', type, promise);
+            });
+        }
+    },
+    quit: () => {
+        process.quit();
+    },
+}
